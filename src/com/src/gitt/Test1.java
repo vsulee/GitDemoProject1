2ad55fd2eddbe23c8cb81cc155396f79aec35b5e
user <user@DESKTOP-9RFK10M>
2022-12-25 22:24:30 +0530

    First Commit

---
+++ src/com/src/gitt/Test1.java
@@ -0,0 +1,14 @@
+package com.src.gitt;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry=="yes") 
+			System.out.println("eat something");
+			else {
+				System.out.println("do work");
+			}
+	}
+
+}
