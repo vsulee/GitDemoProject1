1d7e1239a12e8b6bc5f81b9f37ba473e3145c5b0
user <user@DESKTOP-9RFK10M>
2022-12-26 19:26:57 +0530

    Added additional checks....

--- src/com/src/gitt/Test2.java
+++ src/com/src/gitt/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		String hungry = "yes";
-		if(hungry=="yes") 
+		if(hungry=="yes" || hungry == "Y" || hungry=="y") 
 			System.out.println("eat something");
 			else {
 				System.out.println("do workkkkkk");
